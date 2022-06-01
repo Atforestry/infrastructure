c4985a62ae12270d67c911465354bfe810b606a4
Borja Gómez Muñoz <borja@reverscore.com>
2022-06-01 15:55:04 +0200

    Change schema to incorporate 2 squares. Adding user and password to postgres

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
 -- postgres
 
-create database atforestry;
-
 CREATE TABLE IF NOT EXISTS Prediction (
   id serial PRIMARY KEY,
-  longitude double precision NOT NULL,
-  latitude double precision NOT NULL,
+  sqbl_longitude double precision NOT NULL,
+  sqbl_latitude double precision NOT NULL,
+  sqtr_longitude double precision NOT NULL,
+  sqtr_latitude double precision NOT NULL,
   prediction VARCHAR(10) NOT NULL,
   predictionTimestamp TIMESTAMP NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
